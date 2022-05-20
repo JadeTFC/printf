ad7ad7eb6871222f8920dc3b14815cca4a96c909
JadeTFC <101394926+JadeTFC@users.noreply.github.com>
2022-05-20 13:07:59 +0100

    printf.c

--- _printf.c
+++ _printf.c
@@ -3,41 +3,84 @@
 #include <stdarg.h>
 
 /**
- *_printf - recreates printf functions
- *@format: string with format specifiers
- *Return: number of printed chars
+ * get_op - select function for conversion char
+ * @c: char to check
+ * Return: pointer to function
+ */
+
+int (*get_op(const char c))(va_list)
+{
+	int i = 0;
+
+	flags_p fp[] = {
+		{"c", print_char},
+		{"s", print_str},
+		{"i", print_nbr},
+		{"d", print_nbr},
+		{"b", print_binary},
+		{"o", print_octal},
+		{"x", print_hexa_lower},
+		{"X", print_hexa_upper},
+		{"u", print_unsigned},
+		{"S", print_str_unprintable},
+		{"r", print_str_reverse},
+		{"p", print_ptr},
+		{"R", print_rot13},
+		{"%", print_percent}
+	};
+	while (i < 14)
+	{
+		if (c == fp[i].c[0])
+		{
+			return (fp[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+/**
+ * _printf - Reproduce behavior of printf function
+ * @format: format string
+ * Return: value of printed chars
  */
 
 int _printf(const char *format, ...)
 {
-	register short len = 0;
-	int (*printFunc)(va_list, mods *);
-	mods prefixes = PF_INIT;
-	const char *p = format;
-	va_list arguments;
+	va_list ap;
+	int sum = 0, i = 0;
+	int (*func)();
 
-	va_start(arguments, format);
-	assert(invalidInputs(p));
-	for (; *p; p++)
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	va_start(ap, format);
+
+	while (format[i])
 	{
-		if (*p == '%')
+		if (format[i] == '%')
 		{
-			p++;
-			if (*p == '%')
+			if (format[i + 1] != '\0')
+				func = get_op(format[i + 1]);
+			if (func == NULL)
 			{
-				len += _putchar('%');
+				_putchar(format[i]);
+				sum++;
+				i++;
+			}
+			else
+			{
+				sum += func(ap);
+				i += 2;
 				continue;
 			}
-			while (get_flags(*p, &prefixes))
-				p++;
-			printFunc = get_print(*p);
-			len += (printFunc)
-				? printFunc(arguments, &prefixes)
-				: _printf("%%%c", *p);
-		} else
-			len += _putchar(*p);
+		}
+		else
+		{
+			_putchar(format[i]);
+			sum++;
+			i++;
+		}
 	}
-	_putchar(FLUSH);
-	va_end(arguments);
-	return (len);
+	va_end(ap);
+	return (sum);
 }
